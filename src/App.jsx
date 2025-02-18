e842ef628360be5532655ebf59f387adb21a78d3
paulcostanza <paulvincentcostanza@gmail.com>
2025-02-18 13:12:32 -0500

    first commit: project setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import githubLogo from './assets/github-logo.webp'
+import './App.css'
+import './temp.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Rock Paper Scissor Dash</h1>
+      <div>
+        <a href='https://github.com/joannaromben/CSCI401--Rock-Paper-Scissors-Dash' target='_blank'>
+          <button>
+            <img id="github-logo" src={githubLogo} alt="github-logo" />
+          </button>
+        </a>
+
+        <p>
+          Future site of our game, stay tuned!
+        </p>
+      </div>
+      <p className="read-the-docs">
+
+      </p>
+    </>
+  )
+}
+
+export default App
