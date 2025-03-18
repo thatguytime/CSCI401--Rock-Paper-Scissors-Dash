34ba8098e21de69fa35a3ddcba2af8c6eaef400d
paulcostanza <paulvincentcostanza@gmail.com>
2025-03-18 14:18:05 -0400

    Home.jsx: fixed typo, take two

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react"
 import runCanvas from '../maps/levels.js'
 import clock from '../maps/clock.js'
-import LevelSelect from '../maps/LevelSelect.jsx'
+// import LevelSelect from '../maps/LevelSelect.jsx'
 import { Link } from 'react-router-dom'
 
 export default function Canvas(props) {
